cc7bb1e1f09d3b63e2e38e26ce6cba2c7b0e9d56
nickiitsu <pannatorn.po@skru.ac.th>
2017-04-28 14:40:01 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -20,7 +20,7 @@ if (!is_null($events['events'])) {
 			$messages = [
 				'type' => 'text',
 				//'text' => $text
-				'text' => 'ไม่ตอบหรอก อิอิอิ ฝากเพจ:<a href="https://www.facebook.com/Thara.Betta.TH/" target="_blank">กด</a>'
+				'text' => 'ไม่ตอบหรอก อิอิอิ ฝากเพจ:https://www.facebook.com/Thara.Betta.TH/'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
